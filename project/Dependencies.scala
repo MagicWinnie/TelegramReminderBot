9c672d4eaf359a46f513b7b676b640a01fdb1e92
Dmitriy Okoneshnikov <magicwinnie@yandex.ru>
2024-11-28 23:30:04 +0300

    Added telegram and mongodb to dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,16 +3,18 @@ import sbt.*
 object Dependencies {
 
   object core {
-    lazy val catsCore = "org.typelevel" %% "cats-core" % "2.12.0"
+//    lazy val catsCore = "org.typelevel" %% "cats-core" % "2.12.0"
+    lazy val telegramCore = "com.bot4s" %% "telegram-core" % "5.8.3"
+    lazy val mongoScalaDriver = "org.mongodb.scala" %% "mongo-scala-driver" % "5.2.0"
 
-    lazy val all: Seq[ModuleID] = Seq(catsCore)
+    lazy val all: Seq[ModuleID] = Seq(telegramCore, mongoScalaDriver)
   }
 
   object test {
     lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.19" % Test
-    lazy val catsLaws = "org.typelevel" %% "cats-laws" % "2.12.0" % Test
-    lazy val disciplineScalatest = "org.typelevel" %% "discipline-scalatest" % "2.3.0" % Test
+//    lazy val catsLaws = "org.typelevel" %% "cats-laws" % "2.12.0" % Test
+//    lazy val disciplineScalatest = "org.typelevel" %% "discipline-scalatest" % "2.3.0" % Test
 
-    lazy val all: Seq[ModuleID] = Seq(scalaTest, catsLaws, disciplineScalatest)
+    lazy val all: Seq[ModuleID] = Seq(scalaTest)
   }
 }
