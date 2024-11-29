c464fbd1b5deadba8b8705d809a1500c54e3b601
Dmitriy Okoneshnikov <magicwinnie@yandex.ru>
2024-11-29 09:45:10 +0300

    Added logging to dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,8 +6,10 @@ object Dependencies {
 //    lazy val catsCore = "org.typelevel" %% "cats-core" % "2.12.0"
     lazy val telegramCore = "com.bot4s" %% "telegram-core" % "5.8.3"
     lazy val mongoScalaDriver = "org.mongodb.scala" %% "mongo-scala-driver" % "5.2.0"
+    lazy val slogging = "biz.enef" %% "slogging" % "0.6.2"
+    lazy val slf4jSimple = "org.slf4j" % "slf4j-simple" % "2.0.13"
 
-    lazy val all: Seq[ModuleID] = Seq(telegramCore, mongoScalaDriver)
+    lazy val all: Seq[ModuleID] = Seq(telegramCore, mongoScalaDriver, slogging, slf4jSimple)
   }
 
   object test {
