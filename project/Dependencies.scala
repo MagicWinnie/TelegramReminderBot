2a456903d5572497e4875d7ccf65219fed87491f
Dmitriy Okoneshnikov <magicwinnie@yandex.ru>
2024-11-29 10:18:37 +0300

    Added cats-effect and sttp to dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,13 +3,18 @@ import sbt.*
 object Dependencies {
 
   object core {
-//    lazy val catsCore = "org.typelevel" %% "cats-core" % "2.12.0"
+    lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.5.7"
     lazy val telegramCore = "com.bot4s" %% "telegram-core" % "5.8.3"
     lazy val mongoScalaDriver = "org.mongodb.scala" %% "mongo-scala-driver" % "5.2.0"
+
     lazy val slogging = "biz.enef" %% "slogging" % "0.6.2"
     lazy val slf4jSimple = "org.slf4j" % "slf4j-simple" % "2.0.13"
 
-    lazy val all: Seq[ModuleID] = Seq(telegramCore, mongoScalaDriver, slogging, slf4jSimple)
+    lazy val sttpCore = "com.softwaremill.sttp.client3" %% "core" % "3.9.7"
+    lazy val sttpAsync = "com.softwaremill.sttp.client3" %% "async-http-client-backend-cats" % "3.9.7"
+
+    lazy val all: Seq[ModuleID] =
+      Seq(catsEffect, telegramCore, mongoScalaDriver, slogging, slf4jSimple, sttpCore, sttpAsync)
   }
 
   object test {
