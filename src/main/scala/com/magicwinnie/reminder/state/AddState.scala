8e0c7ae93d12c799c98af171d9858f6253d21f64
Dmitriy Okoneshnikov <magicwinnie@yandex.ru>
2024-11-29 14:21:21 +0300

    Added channel state per chat

---
+++ src/main/scala/com/magicwinnie/reminder/state/AddState.scala
@@ -0,0 +1,6 @@
+package com.magicwinnie.reminder.state
+
+object AddState extends Enumeration {
+  type AddState = Value
+  val Name, Date, Repeat = Value
+}
---
+++ src/main/scala/com/magicwinnie/reminder/state/PerChatState.scala
@@ -0,0 +1,27 @@
+package com.magicwinnie.reminder.state
+
+import com.bot4s.telegram.models.Message
+
+import scala.concurrent.Future
+
+trait PerChatState[S] {
+  private val chatState = collection.mutable.Map[Long, S]()
+
+  def setChatState(value: S)(implicit msg: Message): Unit = atomic {
+    chatState.update(msg.chat.id, value)
+  }
+
+  def clearChatState(implicit msg: Message): Unit = atomic {
+    chatState.remove(msg.chat.id)
+  }
+
+  private def atomic[T](f: => T): T = chatState.synchronized {
+    f
+  }
+
+  def withChatState(f: Option[S] => Future[Unit])(implicit msg: Message): Future[Unit] = f(getChatState)
+
+  private def getChatState(implicit msg: Message): Option[S] = atomic {
+    chatState.get(msg.chat.id)
+  }
+}
