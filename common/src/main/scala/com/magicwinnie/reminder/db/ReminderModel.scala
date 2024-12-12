bd660dc2389adf9884f7c0e7ca944e33ca07431a
Dmitriy Okoneshnikov <magicwinnie@yandex.ru>
2024-12-12 23:57:12 +0700

    Added comments for ReminderModel

--- common/src/main/scala/com/magicwinnie/reminder/db/ReminderModel.scala
+++ common/src/main/scala/com/magicwinnie/reminder/db/ReminderModel.scala
@@ -5,9 +5,9 @@ import org.mongodb.scala.bson.ObjectId
 
 case class ReminderModel(
   _id: ObjectId = new ObjectId(),
-  chatId: Long,
-  name: String,
-  executeAt: DateTime,
-  repeatIn: Option[Period],
-  timezoneID: String
+  chatId: Long, // ID Telegram чата пользователя
+  name: String, // Название напоминания
+  executeAt: DateTime, // Время напоминания
+  repeatIn: Option[Period], // Период повторения напоминания, None в случае одноразового напоминания
+  timezoneID: String // Joda ID часового пояса
 )
