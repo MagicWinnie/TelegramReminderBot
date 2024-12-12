a782ea418549ee734f7f4a741f579fc69bdfafdf
Dmitriy Okoneshnikov <magicwinnie@yandex.ru>
2024-12-12 23:34:24 +0700

    Added comments for UserState

--- common/src/main/scala/com/magicwinnie/reminder/state/UserState.scala
+++ common/src/main/scala/com/magicwinnie/reminder/state/UserState.scala
@@ -4,10 +4,29 @@ import com.github.nscala_time.time.Imports.DateTime
 
 sealed trait UserState
 
+/** Объект UserState определяет состояния пользователя в системе, а также связанные с ними данные, до записи их в базу
+  * данных.
+  */
 object UserState {
+
+  /** Представляет собой состояние пользователя, в котором мы находимся в ожидании от него названия напоминания.
+    */
   case object AwaitingName extends UserState
 
+  /** Представляет собой состояние пользователя, в котором мы находимся в ожидании от него времени напоминания.
+    *
+    * @param name
+    *   Уже полученное название напоминания
+    */
   case class AwaitingDate(name: String) extends UserState
 
+  /** Представляет собой состояние пользователя, в котором мы находимся в ожидании от него количества дней, через
+    * которое нужно повторить напоминание.
+    *
+    * @param name
+    *   Уже полученное название напоминания
+    * @param executeAt
+    *   Уже полученный объект DateTime, указывающий время напоминания.
+    */
   case class AwaitingRepeat(name: String, executeAt: DateTime) extends UserState
 }
