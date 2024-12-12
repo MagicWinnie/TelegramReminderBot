67c95655f71ec26e50ddb2961209196d38011f78
Dmitriy Okoneshnikov <magicwinnie@yandex.ru>
2024-12-12 23:40:53 +0700

    Added comments for MongoDBClient

--- common/src/main/scala/com/magicwinnie/reminder/db/MongoDBClient.scala
+++ common/src/main/scala/com/magicwinnie/reminder/db/MongoDBClient.scala
@@ -7,9 +7,15 @@ import org.mongodb.scala.{MongoClient, MongoCollection}
 
 import scala.reflect.ClassTag
 
+/** Класс для работы с MongoDB.
+  *
+  * @param uri
+  *   ссылка для подключения к MongoDB.
+  */
 class MongoDBClient(val uri: String) {
   private val client = MongoClient(uri)
 
+  // Регистрация кодеков для сериализации и десериализации объектов DateTime и Period при работе с MongoDB
   private val codecRegistry = fromRegistries(
     fromCodecs(NscalaTimeCodecs.dateTimeCodec, NscalaTimeCodecs.periodCodec),
     fromProviders(classOf[ReminderModel]),
@@ -19,6 +25,15 @@ class MongoDBClient(val uri: String) {
   private val database: IO[org.mongodb.scala.MongoDatabase] =
     IO(client.getDatabase("reminder_bot").withCodecRegistry(codecRegistry))
 
+  /** Метод для получения коллекции из базы данных MongoDB.
+    *
+    * @param name
+    *   имя коллекции, которую необходимо получить
+    * @tparam T
+    *   модель документа
+    * @return
+    *   экземпляр класса IO, содержащий MongoCollection заданного типа
+    */
   def getCollection[T: ClassTag](name: String): IO[MongoCollection[T]] =
     database.map(_.getCollection[T](name))
 }
