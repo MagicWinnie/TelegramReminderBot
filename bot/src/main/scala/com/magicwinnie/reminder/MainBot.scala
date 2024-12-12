3fb689828f7f57497461ba0b4dbf89cda621a7c3
Dmitriy Okoneshnikov <magicwinnie@yandex.ru>
2024-12-13 00:51:10 +0700

    Added comments for MainBot

--- bot/src/main/scala/com/magicwinnie/reminder/MainBot.scala
+++ bot/src/main/scala/com/magicwinnie/reminder/MainBot.scala
@@ -5,8 +5,19 @@ import com.magicwinnie.reminder.bot.Bot
 import com.magicwinnie.reminder.db.{MongoDBClient, ReminderModel, ReminderRepository}
 import com.magicwinnie.reminder.state.{PerChatState, UserState}
 
+/** Main entry point for the Reminder Telegram Bot application.
+  *
+  * Initializes the bot with a Telegram token and MongoDB connection.
+  */
 object MainBot extends IOApp {
 
+  /** Configures and starts the Telegram Bot with MongoDB integration.
+    *
+    * @param args
+    *   Command-line arguments (Telegram bot token, MongoDB URI)
+    * @return
+    *   IO effect representing the application's exit status
+    */
   def run(args: List[String]): IO[ExitCode] = {
     args match {
       case List(token, mongoUri) =>
